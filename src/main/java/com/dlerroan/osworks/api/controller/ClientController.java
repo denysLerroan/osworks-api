d0e570d4e50f609482993c1c130f2e31504c633c
DenysFerreira <lerroandeco@gmail.com>
2020-04-22 17:00:44 -0300

    EntityManager ClientController

--- src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
+++ src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
@@ -1,7 +1,9 @@
 package com.dlerroan.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,12 @@ import com.dlerroan.osworks.domain.model.Client;
 @RestController
 public class ClientController {
 	
+	@PersistenceContext
+	private EntityManager manager; // Responsavel por buscar no banco de dados
+	
 	@GetMapping("/clientes")
 	public List<Client> list() {
-		var c1 = new Client();
-		c1.setId(1L);
-		c1.setName("João");
-		c1.setEmail("joao@gmail.com");
-		c1.setPhone("11 99999-8877");
-		
-		var c2 = new Client();
-		c2.setId(2L);
-		c2.setName("Maria");
-		c2.setEmail("maria@gmail.com");
-		c2.setPhone("11 99988-6644");
-		return Arrays.asList(c1, c2);
+		return manager.createQuery("from Client", Client.class).getResultList();
 	}
 
 }
