2c7cf427212ab8ac41fb5fb1d9a38bcc2decd623
DenysFerreira <lerroandeco@gmail.com>
2020-04-22 17:46:03 -0300

    Client insert

--- src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
+++ src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
@@ -4,26 +4,32 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dlerroan.osworks.domain.model.Client;
 import com.dlerroan.osworks.domain.repository.ClientRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClientController {
 	
 	@Autowired
 	private ClientRepository clientRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Client> list() {
 		return clientRepository.findAll();
 	}
 	
-	@GetMapping("/clientes/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<Client> findById(@PathVariable Long id) {
 		Optional<Client> client = clientRepository.findById(id);
 		
@@ -32,6 +38,12 @@ public class ClientController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client insert(@RequestBody Client client) {
+		return clientRepository.save(client);
 	}
 	
 }
