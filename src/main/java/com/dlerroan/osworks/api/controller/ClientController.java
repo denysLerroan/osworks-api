e8f5c50549963f56c9744d335b21f37e867210e5
DenysFerreira <lerroandeco@gmail.com>
2020-04-22 17:34:28 -0300

    Client FindById with ResponseEntity

--- src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
+++ src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +24,14 @@ public class ClientController {
 	}
 	
 	@GetMapping("/clientes/{id}")
-	public Client findById(@PathVariable Long id) {
+	public ResponseEntity<Client> findById(@PathVariable Long id) {
 		Optional<Client> client = clientRepository.findById(id);
-		return client.orElse(null);
+		
+		if(client.isPresent()) {
+			return ResponseEntity.ok(client.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }
