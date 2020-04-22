57762367681e5db5d1568cc7369d3a362a96ed9c
DenysFerreira <lerroandeco@gmail.com>
2020-04-22 18:20:40 -0300

    Client update

--- src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
+++ src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -44,6 +45,18 @@ public class ClientController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Client insert(@RequestBody Client client) {
 		return clientRepository.save(client);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client){
+		if(!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		client.setId(id);
+		client = clientRepository.save(client);
+		
+		return ResponseEntity.ok(client);
 	}
 	
 }
