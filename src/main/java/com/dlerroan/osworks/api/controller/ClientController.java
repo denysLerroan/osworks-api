0330441e6c8b54049064065b1c79b32eac8d9368
DenysFerreira <lerroandeco@gmail.com>
2020-04-22 18:26:50 -0300

    Client delete

--- src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
+++ src/main/java/com/dlerroan/osworks/api/controller/ClientController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,17 @@ public class ClientController {
 		client = clientRepository.save(client);
 		
 		return ResponseEntity.ok(client);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		if(!clientRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clientRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
