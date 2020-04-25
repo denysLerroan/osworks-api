421f0441449c2657d32f548b7c8bf6056882053e
DenysFerreira <lerroandeco@gmail.com>
2020-04-24 22:17:12 -0300

    ServiceOrder insert with costumer association & exception handler

--- src/main/java/com/dlerroan/osworks/domain/service/ManagmentServiceOrderService.java
+++ src/main/java/com/dlerroan/osworks/domain/service/ManagmentServiceOrderService.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dlerroan.osworks.domain.exception.BusinessException;
+import com.dlerroan.osworks.domain.model.Client;
 import com.dlerroan.osworks.domain.model.ServiceOrder;
 import com.dlerroan.osworks.domain.model.ServiceOrderStatus;
+import com.dlerroan.osworks.domain.repository.ClientRepository;
 import com.dlerroan.osworks.domain.repository.ServiceOrderRepository;
 
 @Service
@@ -15,7 +18,14 @@ public class ManagmentServiceOrderService {
 	@Autowired
 	private ServiceOrderRepository repository;
 	
+	@Autowired
+	private ClientRepository cRepository;
+	
 	public ServiceOrder insert(ServiceOrder serviceOrder) {
+		Client client = cRepository.findById(serviceOrder.getClient().getId())
+				.orElseThrow(() -> new BusinessException("Cliente não encontrado"));
+		
+		serviceOrder.setClient(client);
 		serviceOrder.setStatus(ServiceOrderStatus.ABERTA);
 		serviceOrder.setOpenedDate(LocalDateTime.now());
 		
