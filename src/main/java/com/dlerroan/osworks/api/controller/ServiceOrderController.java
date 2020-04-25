7a0d232919381306d30c857a073b9c3cc7cd6133
DenysFerreira <lerroandeco@gmail.com>
2020-04-24 23:04:59 -0300

    ServiceOrder findAll

--- src/main/java/com/dlerroan/osworks/api/controller/ServiceOrderController.java
+++ src/main/java/com/dlerroan/osworks/api/controller/ServiceOrderController.java
@@ -1,9 +1,12 @@
 package com.dlerroan.osworks.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dlerroan.osworks.domain.model.ServiceOrder;
+import com.dlerroan.osworks.domain.repository.ServiceOrderRepository;
 import com.dlerroan.osworks.domain.service.ManagmentServiceOrderService;
 
 @RestController
@@ -20,10 +24,18 @@ public class ServiceOrderController {
 	@Autowired
 	private ManagmentServiceOrderService service;
 	
+	@Autowired
+	private ServiceOrderRepository repository;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ServiceOrder insert(@Valid @RequestBody ServiceOrder serviceOrder) {
 		return service.insert(serviceOrder);
+	}
+	
+	@GetMapping
+	public List<ServiceOrder> list(){
+		return repository.findAll();
 	}
 
 }
